9755de12236d7fed5200badeaf822d3bd3bd94fa
Pranesh-hub <praneshsenthilvel26@gmail.com>
2025-12-13 11:00:05 +0530

    updating head section

--- src/components/TeamHeadSection.jsx
+++ src/components/TeamHeadSection.jsx
@@ -2,37 +2,37 @@ import React from 'react';
 import TeamHeadCard from './TeamHeadCard';
 
 // Example images (replace with real images)
-import teamhead1Img from '../assets/team/head1.jpg';
-import teamhead2Img from '../assets/team/head2.jpeg';
-import teamhead3Img from '../assets/team/head3.jpeg';
+import teamhead1Img from '../assets/team/22head1.jpg';
+import teamhead2Img from '../assets/team/22head2.jpeg';
+import teamhead3Img from '../assets/team/22head3.jpeg';
 
 const teamHeadsData = [
   {
-    name: 'Mayank Agrawal',
-    phone: '9289077004',
-    email: 'mayanka22@iitk.ac.in',
+    name: 'Arbaaz Tanveer',
+    phone: 'xxxxxxxxxx',
+    email: 'arbaazt23@iitk.ac.in',
     image: teamhead1Img,
-    githubLink: 'https://github.com/Mayank534',
-    instagramLink: 'https://www.instagram.com/_.mayank_agrawal._/',
-    linkedinLink: 'https://www.linkedin.com/in/mayank-agrawal-209085254/',
+    githubLink: 'https://github.com/#',
+    instagramLink: 'https://www.instagram.com/#',
+    linkedinLink: 'https://www.linkedin.com/in/#',
   },
   {
-    name: 'Nikhil Gupta',
-    phone: '7982273730',
-    email: 'nikhilg@iitk.ac.in',
+    name: 'Vihaan Sapra',
+    phone: 'xxxxxxxxxx',
+    email: 'svihaan23@iitk.ac.in',
     image: teamhead2Img,
-    githubLink: 'https://github.com/nikhilg2603',
-    instagramLink: 'https://www.instagram.com/nikhil_gupta_36/',
-    linkedinLink: 'https://www.linkedin.com/in/nikhil-gupta-970710279/',
+    githubLink: 'https://github.com/#',
+    instagramLink: 'https://www.instagram.com/#',
+    linkedinLink: 'https://www.linkedin.com/in/#',
   },
   {
-    name: 'Debraj Karmakar',
-    phone: '9470961409',
-    email: 'debrajk@iitk.ac.in',
+    name: 'Himesh Pratap Singh',
+    phone: 'xxxxxxxxxx',
+    email: 'himeshps23@iitk.ac.in',
     image: teamhead3Img,
-    githubLink: 'https://github.com/debrajk22',
-    instagramLink: 'https://www.instagram.com/debraj___k/',
-    linkedinLink: 'https://www.linkedin.com/in/debraj-karmakar-a3758a255/',
+    githubLink: 'https://github.com/#',
+    instagramLink: 'https://www.instagram.com/#',
+    linkedinLink: 'https://www.linkedin.com/in/#',
   },
 ];
 
